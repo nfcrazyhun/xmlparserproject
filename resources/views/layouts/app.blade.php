e185de211a28db17ca57948bc2e7250dde705f9c
Földesi Péter <nfcrazyhun@gmail.com>
2023-06-11 12:28:25 +0200

    add basic layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- CSS -->
+    <script src="https://cdn.tailwindcss.com/3.3.2"></script>
+
+    <!-- JS -->
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.12.x/dist/cdn.min.js"></script>
+
+</head>
+<body class="font-sans antialiased">
+<div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+    @include('layouts.navigation')
+
+    <!-- Page Heading -->
+    @if (isset($header))
+        <header class="bg-white shadow dark:bg-gray-800">
+            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                {{ $header }}
+            </div>
+        </header>
+    @endif
+
+    <!-- Page Content -->
+    <main>
+        {{ $slot }}
+    </main>
+</div>
+</body>
+</html>
---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,22 @@
+<nav x-data="{ open: false }" class="border-b border-gray-100 bg-white dark:border-gray-700 dark:bg-gray-800">
+    <!-- Primary Navigation Menu -->
+    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div class="flex h-16 justify-between">
+            <div class="flex">
+                <!-- Logo -->
+                <div class="flex shrink-0 items-center">
+                    <a href="{{ route('home') }}">
+                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                    </a>
+                </div>
+
+                <!-- Navigation Links -->
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
+                        {{ __('Home') }}
+                    </x-nav-link>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
